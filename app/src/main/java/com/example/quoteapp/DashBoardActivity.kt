5c82e1704162f62e36e01598438fbe8b4816cb6e
Utsav <utsavbudathoki12@gmail.com>
2021-08-04 13:33:35 +0545

    dashboard added

--- app/src/main/java/com/example/quoteapp/DashBoardActivity.kt
+++ app/src/main/java/com/example/quoteapp/DashBoardActivity.kt
@@ -1,11 +1,20 @@
 package com.example.quoteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+    }
+
+    fun onShuffle(view: View) {
+
+        startActivity(Intent(this,ListActivity::class.java))
+
+
     }
 }
