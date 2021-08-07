da37a3bc3c2a7a3ad14673cf7222865820a21738
Utsav <utsavbudathoki12@gmail.com>
2021-08-07 14:31:08 +0545

    dashboard random quote done

--- app/src/main/java/com/example/quoteapp/DashBoardActivity.kt
+++ app/src/main/java/com/example/quoteapp/DashBoardActivity.kt
@@ -4,20 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class DashBoardActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
+
+    private val quoteText: TextView
+        get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(application,"quotes.json"))
+            .get(MainViewModel::class.java)
+        val quote = mainViewModel.randomQuote()
+        setQuote(quote)
+
+
+
+
     }
 
-    fun onShuffle(view: View) {
 
-        Intent(this,ListActivity::class.java).apply {
-            putExtra("name","quotes.json")
-            startActivity(this)
-        }
-
+    private fun setQuote(quote:Quote){
+        quoteText.text = quote.text
+        quoteAuthor.text = quote.author
     }
 
     fun onHope(view: View) {
